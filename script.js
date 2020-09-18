035966524737fae74e998d5276eb8427204de4c2
unknown <mirjalol03>
2020-09-18 14:26:24 +0300

    new animation

--- script.js
+++ script.js
@@ -1,2 +1,61 @@
-const canvas = document.getElementById("canvas1")
-const ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas,height/80) * (canvas.width/80);
+}
+
+window.addEventListener('mousemove',
+    function(event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+):
+
+class Particle {
+    constructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, folse);
+        ctx.fillStyle = '#8c5523';
+        ctx.fill();
+    }
+
+    update() {
+        if (this.x > canvas.width || this.x < 0 ) {
+            this.directionX = - this.directionX;
+        }
+        if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY
+        }
+        
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Mathsqrt(dx*dx +dy*dy);
+        if (distance < mouse.radius + this.size){
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+            }
+            if (mouse.x < this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+            }
+            if (mouse.y < this.y && this.y > this.size * 10) {
+                this.y -= 10;
+        }
+    }
